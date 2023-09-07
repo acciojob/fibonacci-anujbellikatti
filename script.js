4f6f41c31a3e68a09b8c9c3c56411a8e57d9c312
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-08 00:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function fibonacci(num) {
 // your code here
-	function fibonacci(num) {
-  if (num === 0) {
-    return 0;
-  } else if (num === 1 || num === 2) {
-    return 1;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
-  }
-}
-
-	
+	if(num==1){
+		return 0;
+	}
+	if(num==2){
+		return 1;
+	}
+	return fibonacci(num-1)+fibonacci(num-2)
 }
 
 module.exports = fibonacci;
